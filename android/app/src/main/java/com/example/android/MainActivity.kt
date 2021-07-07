6a8c3082713f8d203cfc9bf035581311c2f9eae3
João Cunha <skidrow.jc@gmail.com>
2021-07-07 12:07:57 +0100

    Tabs

--- android/app/src/main/java/com/example/android/MainActivity.kt
+++ android/app/src/main/java/com/example/android/MainActivity.kt
@@ -11,23 +11,26 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 private val FRAGMENTS = arrayOf(
-    Tuple(R.string.tab_name_1, ListTodo()),
-    Tuple(R.string.tab_name_2, ListDone())
+    Tuple(R.string.tab_name_1, R.drawable.ic_close_24, ListTodo()),
+    Tuple(R.string.tab_name_2, R.drawable.ic_done_24, ListDone())
 )
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val tabLayout = findViewById<TabLayout>(R.id.tabs)
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
 
         val adapter = ViewPageAdapter(FRAGMENTS, supportFragmentManager, lifecycle)
-        viewPager.adapter = adapter;
+        viewPager.adapter = adapter
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = getString(FRAGMENTS[position].name)
+            tab.setText(FRAGMENTS[position].name)
+            tab.setIcon(FRAGMENTS[position].icon)
         }.attach()
     }
 }
--- android/app/src/main/java/com/example/android/classes/Tuple.kt
+++ android/app/src/main/java/com/example/android/classes/Tuple.kt
@@ -2,7 +2,8 @@ package com.example.android.classes
 
 import androidx.fragment.app.Fragment
 
-class Tuple(_name: Int, _frag: Fragment) {
+class Tuple(_name: Int, _icon: Int, _frag: Fragment) {
     var name = _name
+    var icon = _icon
     var frag = _frag
 }
